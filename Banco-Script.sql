6a9e69faab6c7c94e0820d06989515931dae1649
Matheus Castro <160545014+Matheus-Castr0@users.noreply.github.com>
2025-05-27 16:44:01 -0300

    Update Banco-Script.sql

--- Banco-Script.sql
+++ Banco-Script.sql
@@ -7,7 +7,8 @@ CREATE TABLE usuario (
     sobrenome VARCHAR(255) NOT NULL,
     cargo VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
+    senha VARCHAR(255) NOT NULL,
+    token varchar(255) NOT NULL
 );
 
 
